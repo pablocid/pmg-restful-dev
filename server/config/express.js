24f4790dd1b7d3cdd3e4529c3e17e5c724635878
pablocid <pabloagronomo@gmailcom>
2016-07-22 15:12:31 -0300

    Built pmg-restful-api from commit (unavailable) on branch (unavailable)

--- server/config/express.js
+++ server/config/express.js
@@ -52,10 +52,12 @@ exports.default = function (app) {
    */
   if (env !== 'test' && !process.env.SAUCE_USERNAME) {
     app.use((0, _lusca2.default)({
+      /*
       csrf: {
         angular: true
-      },
-      xframe: 'SAMEORIGIN',
+      },*/
+      csrf: false,
+      //xframe: 'SAMEORIGIN',
       hsts: {
         maxAge: 31536000, //1 year, in seconds
         includeSubDomains: true,
